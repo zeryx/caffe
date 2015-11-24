024997fd019f43f8f2b2f5cb932a2492b5d2793f
Micah Chambers <micah@plangrid.com>
2015-11-24 10:19:29 -0800

    added setup.py and script to create .deb
    - works on ubuntu 14.04 and archlinux

---
+++ scripts/make_deb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x
+VERSION=1.0rc2-1
+ROOTDIR=caffe_${VERSION}
+
+make all distribute
+mkdir $ROOTDIR
+mkdir -p $ROOTDIR/usr/bin
+mkdir -p $ROOTDIR/usr/lib
+mkdir -p $ROOTDIR/usr/lib/python2.7/site-packages/caffe
+
+mv distribute/include/* $ROOTDIR/usr/include/
+mv distribute/bin/* $ROOTDIR/usr/bin
+mv distribute/python/caffe/* $ROOTDIR/usr/lib/python2.7/site-packages/caffe
+mv distribute/lib/* $ROOTDIR/usr/lib/
+
+# Control
+mkdir $ROOTDIR/DEBIAN
+echo '
+Package: caffe
+Version: 1.0rc2
+Section: mathematics
+Priority: optional
+Architecture: amd64
+Depends: libprotobuf-dev, libleveldb-dev, libsnappy-dev, libopencv-dev, libhdf5-serial-dev, protobuf-compiler, libboost-all-dev, python-dev, libgflags-dev, libgoogle-glog-dev, liblmdb-dev
+Maintainer: Micah Chambers <micahc.vt@gmail.com>
+Description: Caffe is a machine learning toolset.
+
+' > $ROOTDIR/DEBIAN/control
+
+dpkg-deb --build $ROOTDIR
+
